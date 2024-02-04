dbd28787a824f7e5b8ecd348dfdcabbc3630a5db
NuoWenLei <nuowen0612@gmail.com>
2024-02-04 02:56:12 -0500

    optimize build

--- midjourney-telephone/src/app/(main)/games/page.tsx
+++
@@ -1,9 +0,0 @@
-import Searchbar from "@/components/Searchbar";
-
-export default function Game() {
-	return (
-		<main className="flex h-full w-full flex-col items-center justify-between pt-4 pb-4">
-			<Searchbar />
-		</main>
-	)
-}
