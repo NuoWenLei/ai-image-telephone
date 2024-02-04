06b08e2bfc16830b452bb61f8c26674b74b0a342
NuoWenLei <nuowen0612@gmail.com>
2024-02-03 19:23:44 -0500

    DARK FOR EVERYONE

--- midjourney-telephone/src/app/layout.tsx
+++ midjourney-telephone/src/app/layout.tsx
@@ -1,22 +1,22 @@
-import Sidebar from '@/components/Sidebar'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import Navbar from '@/components/Navbar'
-import { AuthProvider } from '@/utilities/firebase/firebaseAuthProvider'
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "@/components/Navbar";
+import { AuthProvider } from "@/utilities/firebase/firebaseAuthProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Midjourney AI Telephone'
-}
+  title: "Midjourney AI Telephone",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -24,25 +24,23 @@ export default function RootLayout({
         <body className={"flex flex-row bg-black " + inter.className}>
           <div className="flex flex-col">
             <Navbar />
-            <div className="flex flex-row w-full h-full">
-              {children}
-            </div>
+            <div className="flex flex-row w-full h-full">{children}</div>
           </div>
           <Sidebar />
           <ToastContainer
-          position="bottom-left"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
+            position="bottom-left"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
           />
-          </body>
-        </AuthProvider>
+        </body>
+      </AuthProvider>
     </html>
-  )
+  );
 }
